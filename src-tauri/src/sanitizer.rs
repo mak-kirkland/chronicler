62df813ef20eb37c64071414b9ebe3657d93927c
Michael Kirkland <mak.kirkland@proton.me>
2025-10-08 21:01:12 +0200

    refactor(sanitizer): Add support for more table attributes
    Allows the use of `width`, `cellspacing`, and `cellpadding` on
    table-related elements to provide users with more layout control.

--- src-tauri/src/sanitizer.rs
+++ src-tauri/src/sanitizer.rs
@@ -62,8 +62,8 @@ pub fn sanitize_html(dirty_html: &str) -> String {
         .add_tag_attributes("span", &["class"])
         .add_tag_attributes("details", &["open"])
         .add_tag_attributes("abbr", &["title"]) // Allow title for abbreviations
-        .add_tag_attributes("th", &["style", "align", "valign"]) // Allow table header alignment
-        .add_tag_attributes("td", &["style", "align", "valign"]) // Allow table cell alignment
+        .add_tag_attributes("th", &["style", "align", "valign", "width"]) // Allow table header alignment
+        .add_tag_attributes("td", &["style", "align", "valign", "width"]) // Allow table cell alignment
         // Allow 'id' attribute on all heading tags for TOC linking.
         .add_tag_attributes("h1", &["id"])
         .add_tag_attributes("h2", &["id"])
@@ -71,7 +71,10 @@ pub fn sanitize_html(dirty_html: &str) -> String {
         .add_tag_attributes("h4", &["id"])
         .add_tag_attributes("h5", &["id"])
         .add_tag_attributes("h6", &["id"])
-        .add_tag_attributes("table", &["border", "align"]) // Allow border and align on tables
+        .add_tag_attributes(
+            "table",
+            &["border", "align", "width", "cellspacing", "cellpadding"],
+        )
         .clean(dirty_html)
         .to_string()
 }
