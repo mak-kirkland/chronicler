d932fb8eaeec756a6b4b4e7822c0c4d0a5e87b7f
Michael Kirkland <mak.kirkland@proton.me>
2025-11-26 22:35:12 +0100

    feat(help): Add button to open the Help page in the browser

--- src/lib/components/HelpModal.svelte
+++ src/lib/components/HelpModal.svelte
@@ -6,6 +6,8 @@
     import Preview from "./Preview.svelte";
     import ErrorBox from "./ErrorBox.svelte";
     import type { RenderedPage } from "$lib/bindings";
+    import Button from "./Button.svelte";
+    import { openUrl } from "@tauri-apps/plugin-opener";
 
     let { onClose } = $props<{ onClose: () => void }>();
 
@@ -33,6 +35,10 @@
     {:else if error}
         <ErrorBox>{error}</ErrorBox>
     {:else if renderedData}
+        <Button
+            onclick={() => openUrl("https://chronicler.pro/getting-started")}
+            >Open in Browser</Button
+        >
         <Preview {renderedData} />
     {/if}
 </Modal>
